1ed19971ded9e49fe8890bbb38e7ff2d1398c5dc
Ashley Wright <AshleyMarieEvans@gmail.com>
2013-12-20 11:38:13 -0600

    Added tests for Year class

--- test/test_cal.rb
+++ test/test_cal.rb
@@ -87,6 +87,28 @@ class TestCal < MiniTest::Unit::TestCase
     assert_equal [15, 16, 17, 18, 19, 20, 21], january.calendar_row[3]
   end
 
+  def test_X_Year_class_takes_one_argument
+    year = Year.new(2012)
+    assert_instance_of(Year, year)
+  end
+
+  def test_X_Year_class_leap_year
+    year = Year.new(2012)
+    assert_equal true, year.leap_year?
+  end
+
+  def test_X_Year_class_not_leap_year
+    year = Year.new(2013)
+    assert_equal false, year.leap_year?
+  end
+
+  def test_X_Year_class_calendar_row
+    year = Year.new(2012)
+    row = "15 16 17 18 19 20 21  12 13 14 15 16 17 18  11 12 13 14 15 16 17"
+    assert_equal row, year.calendar_row[3]
+  end
+
+
   # Integration Testing
 
   def test_10_print_month_starts_on_sunday
